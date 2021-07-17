f0e9c2e2a3e5c239f3570e163e813d352cecf7e7
IvanVnucec <vnucec.ivan@gmail.com>
2021-07-17 13:55:40 +0200

    project points on 2d plane

--- parser.py
+++ parser.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.lib.function_base import diff
 
 
 def spher_to_cart(p):
@@ -33,7 +32,7 @@ def plot_on_sphere(points):
     plt.show()    
 
 
-def plot_on_sphere_inside_FOV(points, camera, FOV):
+def stars_inside_FOV(points, camera, FOV):
     points_inside = []
     for point in points:
         # camera
@@ -51,27 +50,51 @@ def plot_on_sphere_inside_FOV(points, camera, FOV):
         if angle < np.deg2rad(FOV/2):
             points_inside.append(point)
 
-    plot_on_sphere(np.array(points_inside))
+    return np.vstack(points_inside)
+
+
+def q_to_R(q):
+    # q = [[e] 1]
+    q0 = q[3]
+    q1 = q[0]
+    q2 = q[1]
+    q3 = q[2]
+
+    R11 = 2 * (q0**2 + q1**2) - 1
+    R12 = 2 * (q1*q2 - q0*q3)
+    R13 = 2 * (q1*q3 + q0*q2)
+    R21 = 2 * (q1*q2 + q0*q3)
+    R22 = 2 * (q0**2 + q2**2) - 1
+    R23 = 2 * (q2*q3 - q0*q1)
+    R31 = 2 * (q1*q3 - q0*q2)
+    R32 = 2 * (q2*q3 + q0*q1)
+    R33 = 2 * (q0**2 + q3**2) - 1
+
+    return np.block([
+        [R11, R12, R13],
+        [R21, R22, R23],
+        [R31, R32, R33]
+    ])
 
 
 # read from the star catalog
 with open('data/catalog.dat') as f:
     #lines = f.readlines()
-    lines = [f.readline() for _ in range(10000)]
+    lines = [f.readline() for _ in range(100000)]
 
 # extract the star positions from the catalog
 stars_list = [(float(line[153:164]), float(line[167:177])) for line in lines]
 stars = np.array(stars_list)
 
 # plot points on the sphere
-plot_on_sphere(stars)
+#plot_on_sphere(stars)
 
 # camera
 FOV = 20.0 # degrees
 
 # camera orientation
 # TODO: pick some random orientation
-camera = np.array([np.pi/3, -1.0])
+camera = np.array([np.pi/6, -1.0])
 
 r = 1.0
 phi, theta = camera[0], camera[1]
@@ -82,4 +105,60 @@ e = np.array([
 q = np.vstack((e, 1.0))
 
 # plot points on the sphere within FOV
-plot_on_sphere_inside_FOV(stars, camera, FOV)
+stars_fov = stars_inside_FOV(stars, camera, FOV)
+plot_on_sphere(stars_fov)
+
+# project points on the sphere to 2D frame
+f = 0.1 # meters
+rhou = 0.0001 # pixel sizes in meters
+rhov = 0.0001
+u0 = 0.01 # pixel locations of the origin of the image plane in meters
+v0 = 0.01
+
+Maff = np.array([
+    [1/rhou, 0,    u0],
+    [0,   -1/rhov, v0],
+    [0,      0,     1]])
+print('Maff\n', Maff)
+
+Mproj = np.array([
+    [f, 0, 0, 0],
+    [0, f, 0, 0],
+    [0, 0, 1, 0]])
+print('Mproj\n', Mproj)
+
+tc = np.array([0.0, 0.0, 0.0]) # camera coordiantes in intertial frame
+Rc = q_to_R(q) # quaternion to rot. matrix
+
+Mext11 = Rc.T
+Mext12 = - Rc.T.dot(tc).reshape(3, 1)
+Mext21 = np.zeros((1, 3))
+Mext22 = 1.0
+Mext = np.block([
+    [Mext11, Mext12],
+    [Mext21, Mext22]])
+print('Mext\n', Mext)
+
+M = Maff.dot(Mproj).dot(Mext)
+print('M\n', M)
+
+stars_2d = np.empty((len(stars_fov), 3)) # in the end should be [u, v, 1]
+for i, star in enumerate(stars_fov):
+    star_spher_coord = (star[0], star[1], 1.0) 
+    U, V, W = spher_to_cart(star_spher_coord)
+    point = np.array([U, V, W, 1])
+    proj = M.dot(point)
+    stars_2d[i] = proj
+
+print('stars_2d\n', stars_2d)
+
+# plot 2D points
+u = stars_2d[:,0]
+v = stars_2d[:,1]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(u, v, s=1)
+ax.set_box_aspect(aspect = 1)
+plt.show()
+
+# TODO: stars_2d
