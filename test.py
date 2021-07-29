461ceeabe41ca475cf125f16290ece77c4da584f
IvanVnucec <vnucec.ivan@gmail.com>
2021-07-29 22:47:34 +0200

    refactor cam proj test

--- test.py
+++ test.py
@@ -2,89 +2,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_2d_points(X, Y, colors=None):
+def get_colors(n):
+    np.random.seed(1337)
+    return np.random.rand(n, 3)
+
+
+def plot_2d_points(X, Y, x0=0.0, y0=0.0):
     fig = plt.figure()
     ax = fig.add_subplot()
     #ax.set_aspect('equal')
-    ax.scatter(X, Y, c=colors)
+    ax.scatter(X, Y, c=get_colors(len(X)))
     #ax.set_xlim([-100, 1100])
     #ax.set_ylim([-100, 2100])
     ax.grid(True)
-    plt.show()
 
 
-def plot_3d_points(X, Y, Z, colors=None):
+def plot_3d_points(X, Y, Z):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     #ax.set_aspect('equal')
-    ax.scatter(X, Y, Z, c=colors)
+    ax.scatter(X, Y, Z, c=get_colors(len(X)))
 
 
-def generate_points(n=1000):
-    x = np.array([0, 1, 1]).reshape((3, 1))
-    y = np.array([0, 1, 2]).reshape((3, 1))
-    z = np.array([1, 1, 1]).reshape((3, 1))
-
-    """
-    r = 1.0
-    theta = np.random.uniform(0, 0.02, (n, 1)) # valid range [0, 2pi]
-    phi =   np.random.uniform(0, 0.02, (n, 1)) # valid range [0,  pi]
-    
-    sin = np.sin
-    cos = np.cos
-
-    x = r * cos(theta) * sin(phi)
-    y = r * sin(theta) * sin(phi)
-    z = r * cos(phi)
-    """
-    return x, y, z
+def to_hom(p):
+    return np.append(p, 1.0)
 
 
-def get_camera_matrix(f, dx, dy, x0, y0, t, R):
-    ax = f/dx
-    ay = f/dy
-
-    # Camera calibration matrix
-    K = np.array([
-        [ax,  0,  x0],
-        [ 0, ay,  y0],
-        [ 0,  0,   1]
-    ])
-
-    # Camera matrix 3x4
-    P_ = np.block([R, t.reshape((3, 1))])
-    P = K.dot(P_)
-
-    return P
+def from_hom(ph):
+    return ph[0:2] / ph[-1]
 
 
-colors = np.array(['r', 'g', 'b'])
 
-# Internal parameters
-f  = 1e-3 # focal length in meters
-dx = 1e-6 # pixel dimension in meters
-dy = 1e-6 # pixel dimension in meters
-x0 = 1e+3 # CCD centre in pixels
-y0 = 1e+3 # CCD centre in pixels
+f = 1e-3                # m
+px = py = 1e-2          # m
+mx = my = 1e6           # pixels/m
+s = 0.0                 # skew
+C = np.zeros((3, 1))    # m
+R = np.eye(3)
 
-# External parameters
-R = np.eye(3)               # World to Camera coords 
-t = np.array([0, 0, 0])     # in Camera coords in meters
+t = -R.dot(C)
 
+ax = f * mx     # pixels
+ay = f * my     # pixels
+x0 = mx * px    # pixels
+y0 = my * py    # pixels
 
-P = get_camera_matrix(f, dx, dy, x0, y0, t, R)
+K = np.array([
+    [ax, s, x0],
+    [0, ay, y0],
+    [0, 0,   1]
+])
 
-X, Y, Z = generate_points()
+P = K.dot(np.block([R, t]))
 
-plot_3d_points(X, Y, Z, colors)
+XYZ = np.array([       # m
+    [ 0,  0,  1],
+    [ 1,  0,  1],
+    [ 0,  1,  1],
+    [-1,  0,  1],
+    [ 0, -1,  1],
+])
 
-XYZ1 = np.hstack((X, Y, Z, np.ones_like(X)))
+XYZ1 = np.empty((XYZ.shape[0], 4))
+for i, xyz in enumerate(XYZ):
+    XYZ1[i] = to_hom(xyz)
 
-xy1 = np.empty((XYZ1.shape[0], 3))
-# Points in pixel
-for i, xyz1 in enumerate(XYZ1):
-    xy1_ = P.dot(xyz1)
-    xy1[i] = xy1_ / xy1_[-1]
+x = np.empty((XYZ1.shape[0], 3))
+for i, X in enumerate(XYZ1):
+    x[i] = P.dot(X.T)
 
-print(xy1)
-plot_2d_points(xy1[:,0], xy1[:,1], colors)
+PxPy = np.empty(((XYZ1.shape[0], 2)))
+for i, x_hom in enumerate(x):
+    PxPy[i] = from_hom(x_hom)
+
+X, Y, Z = XYZ.T
+plot_3d_points(X, Y, Z)
+
+Px, Py = PxPy.T
+plot_2d_points(Px, Py)
+
+plt.show()
