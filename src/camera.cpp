8df08a9e4cd3105d14488b97880e15ad536fa0b4
Ivan Vnucec <vnucec.ivan@gmail.com>
2022-07-12 11:43:18 +0200

    add camera files

---
+++ src/camera.cpp
@@ -0,0 +1,20 @@
+#include "camera.hpp"
+
+using Eigen::MatrixXd;
+
+Camera::Camera() :
+m_ccd{ MatrixXd::Zero(m_pixel_w, m_pixel_h) }
+{
+}
+
+void Camera::capture(const std::vector<Star>& stars)
+{
+    // TODO
+    m_ccd = MatrixXd::Zero(m_pixel_w, m_pixel_h);
+}
+
+CameraCCD Camera::get_ccd()
+{
+    return m_ccd;
+}
+
