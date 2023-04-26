c45f0755e6c232dc103846a3ec28f926f53374e6
Ivan Vnučec <50591550+IvanVnucec@users.noreply.github.com>
2023-04-26 12:42:25 +0200

    load star catalog

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::{collections::HashMap, time::Duration};
+
 use minifb::{Key, Window, WindowOptions};
 use nalgebra::DMatrix;
 
 const WIDTH: usize = 500;
 const HEIGHT: usize = 500;
+const STARS_CATALOG_PATH: &str = "catalog/hygdata_v3.csv";
+
+type Record = HashMap<String, String>;
 
 fn main() {
+    println!("Loading the Star catalog...");
+    let mut rdr = csv::Reader::from_path(STARS_CATALOG_PATH).unwrap();
+    let records: Vec<Record> = rdr.deserialize().map(Result::unwrap).collect();
+    println!("Done. Number of catalog items: {}", records.len());
+
     let canvas = DMatrix::repeat(HEIGHT, WIDTH, 1000);
 
     let mut window = Window::new("Simulator", WIDTH, HEIGHT, WindowOptions::default())
@@ -13,7 +23,7 @@ fn main() {
         });
 
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    window.limit_update_rate(Some(Duration::from_micros(16600)));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window
