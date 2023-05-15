cdcea1481d4c8d3ab6d8b48575c5368f346767f3
Ivan Vnucec <ivan.vnucec@artronic.net>
2023-05-15 11:33:29 +0200

    add star projection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, time::Duration};
 
 use minifb::{Key, Window, WindowOptions};
-use nalgebra::{DMatrix, Vector3};
+use nalgebra::{DMatrix, Matrix1x3, Matrix3, Matrix3x1, Vector3, Vector2};
 
 #[derive(Debug)]
 struct Star {
@@ -38,20 +38,73 @@ impl StarCatalog {
 struct ImageSensor {
     /// sensor size in Width x Height pixels
     size: (usize, usize),
+    pixel_resolution: (f64, f64),
+    focal_length: f64,
     /// captured image arranged in column-by-columns 10 bit values
     image: DMatrix<u32>,
 }
 
 impl ImageSensor {
-    fn new(size: (usize, usize)) -> Self {
+    fn new(size: (usize, usize), pixel_resolution: (f64, f64), focal_length: f64) -> Self {
         Self {
             size,
+            pixel_resolution,
+            focal_length,
             image: DMatrix::zeros(size.0, size.1),
         }
     }
 
-    fn project(&self, fov: &(f64, f64), star: &Star) -> (i32, i32) {
-        (0, 0)
+    fn project(&self, boresight: &(f64, f64), star: &Star) -> (i32, i32) {
+        let (ra, dec) = star.orientation;
+        let (ra0, dec0) = boresight;
+        let psi = 0.0_f64.to_radians();
+
+        let a = Matrix1x3::new(ra.cos() * dec.cos(), ra.sin() * dec.cos(), dec.sin());
+        let b = Matrix3::new(
+            psi.cos(),
+            psi.sin(),
+            0.0,
+            -psi.sin(),
+            psi.cos(),
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        );
+        let c = Matrix3::new(
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            dec0.sin(),
+            dec0.cos(),
+            0.0,
+            -dec0.cos(),
+            dec0.sin(),
+        );
+        let d = Matrix3::new(
+            -ra0.sin(),
+            ra0.cos(),
+            0.0,
+            -ra0.cos(),
+            -ra0.sin(),
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        );
+        let xyz = a * b * c * d;
+        let xy = self.focal_length / xyz.z * Vector2::new(xyz.x, xyz.y);
+
+        let (sx, sy) = self.pixel_resolution;
+        let (ox, oy) = self.size;
+
+        let uv = (
+            (-xy.x / sx + ox as f64) as i32,
+            (-xy.y / sy + oy as f64) as i32
+        );
+
+        uv
     }
 
     fn capture(&mut self, fov: &(f64, f64), orientation: &(f64, f64), stars: &Vec<Star>) {
@@ -59,7 +112,7 @@ impl ImageSensor {
 
         for s in stars {
             if s.absmag > 5.0 {
-                let (u, v) = self.project(fov, s);
+                let (u, v) = self.project(&orientation, s);
                 if u >= 0 && v >= 0 && u < width as i32 && v < height as i32 {
                     self.image[(u as usize, v as usize)] = 10000;
                 }
@@ -84,12 +137,14 @@ impl StarTracker {
         const FOV_DEG: (f64, f64) = (15.0, 15.0);
         const INITIAL_ORIENTATION: (f64, f64) = (0.0, 0.0);
         const IMAGE_SENSOR_SIZE: (usize, usize) = (640, 480);
+        const PIXEL_RES: (f64, f64) = (10.0, 10.0);
+        const FOCAL_LEN: f64 = 1000.0;
 
         Self {
             fov: (FOV_DEG.0.to_radians(), FOV_DEG.1.to_radians()),
             orientation: INITIAL_ORIENTATION,
             catalog,
-            image_sensor: ImageSensor::new(IMAGE_SENSOR_SIZE),
+            image_sensor: ImageSensor::new(IMAGE_SENSOR_SIZE, PIXEL_RES, FOCAL_LEN),
         }
     }
 
